3ec60d32de6a68d9d0c8535cf1eb337d148d082b
snigdharubama-iitdu <bsse0632@iit.du.ac.bd>
2017-11-08 13:02:54 +0600

    Update mapper.py

---
+++ assignment7/mapper.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+
+import sys
+
+for line in sys.stdin:
+	if(data==1)
+	print data
+	
+
+	for line in sys.stdin:
+    		if(data==2)	
+    		print data
+		else
+		print data=5
+	
+	
+	if HTTPexists:
+	
+	print data=5
